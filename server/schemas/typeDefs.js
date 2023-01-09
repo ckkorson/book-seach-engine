94338db68eb3f393061a44def907f5db4af65dd3
Caleb Korson <caleb@Calebs-MacBook-Air.local>
2023-01-09 18:54:05 -0500

    worked on typeDefs.js

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -9,9 +9,30 @@ type User {
   savedBooks: [Book]
 }
 
+type Book {
+  bookId: ID!
+  authors: [String]
+  description: String
+  title: String!
+  image: String
+  link: String
+}
+
+type Auth {
+  token: ID!
+  user: User
+}
+
 type Query {
   me: User
 }
+
+type Mutation {
+  login(email: String!, password: String!): Auth
+  addUser(username: String!, email: String!, password: String!): Auth
+  saveBook(authors: [String], bookId: String!, )
+}
+
 `;
 
 module.exports = typeDefs;
